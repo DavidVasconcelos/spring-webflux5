6e1a73eaacaa6b736775bd47114bc0db14f108d8
David Vasconcelos <dsv10@hotmail.com>
2023-03-06 11:37:52 -0300

    initial commit

---
+++ stock-trading/src/main/kotlin/com/pluralsight/springwebflux5/stocktrading/StockTradingApplication.kt
@@ -0,0 +1,11 @@
+package com.pluralsight.springwebflux5.stocktrading
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockTradingApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockTradingApplication>(*args)
+}
---
+++ stock-trading/src/test/kotlin/com/pluralsight/springwebflux5/stocktrading/StockTradingApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.pluralsight.springwebflux5.stocktrading
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockTradingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
